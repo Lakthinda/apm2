3f51eeb9826095736429fef108d1229526d70447
Lak <lakthinda@gmail.com>
2018-08-28 11:42:53 +1000

    Added product details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
 import { ProductListComponent } from './products/product-list/product-list/product-list.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -20,15 +22,11 @@ import { ProductListComponent } from './products/product-list/product-list/produ
     HttpClientModule,
     RouterModule.forRoot([
        {path: 'welcome', component: WelcomeComponent},
+       {path: 'products/:id', component: ProductDetailComponent},
        {path: 'products', component: ProductListComponent},
        {path: '', redirectTo: 'welcome', pathMatch: 'full'},
        {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
     ])
-    // RouterModule.forRoot([
-    //   {path: 'welcome', component: WelcomeComponent},
-    //   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-    //   {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    // ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/product-detail/product-detail/product-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../../product';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Details';
+  product: IProduct ;
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+        'productId': id,
+        "productName":"Hammer",
+        "productCode":"TBX-0048",
+        "releaseDate":"May 18, 2018",
+        "description": "Hammer made of Woood",
+        "price": 199.99,
+        "starRating":3.3,
+        "imgUrl":"https://s.hswstatic.com/gif/hammer-1.jpg"
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+
+}
